c6e31fc0d6fbdb0b0c99bb06d82b29ec0818695d
Atif Riaz <a.riaz@cranfield.ac.uk>
2020-11-06 11:12:24 +0000

    Add project files.

---
+++ WebApplication1/Execution/AircraftDesign.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Execution
+{
+    public class AircraftDesign
+    {
+        public void AddNumbers(double x1, double x2, out double y1)
+        {
+            y1 = x1 + x2;
+        }
+
+        public void MultiplyNumbers(double x1, double x2, out double y1)
+        {
+            y1 = x1 * x2;
+        }
+    }
+}
